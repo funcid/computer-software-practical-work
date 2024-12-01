268291dc8ac581d42b00537b302b5bd404c78d1b
Artem Tsaryuk <mps.tsaryuk@crpt.ru>
2024-12-02 00:14:13 +0300

    ok

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,19 +4,20 @@ declare global {
   let prisma: PrismaClient | undefined;
 }
 
-type GlobalWithPrisma = typeof globalThis & {
-  prisma: PrismaClient | undefined;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-let prisma: PrismaClient;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!(global as GlobalWithPrisma).prisma) {
-    (global as GlobalWithPrisma).prisma = new PrismaClient();
-  }
-  prisma = (global as GlobalWithPrisma).prisma as PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
 
 export default prisma; 
